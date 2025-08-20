01bc147e26705f3094978be04755ddc5f51cc87d
Aser Junior <61999214+aserjunior@users.noreply.github.com>
2025-08-20 20:15:32 -0300

    Commit inicial

---
+++ src/test/java/estudos/spring/boot/arquiteturaspring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package estudos.spring.boot.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
